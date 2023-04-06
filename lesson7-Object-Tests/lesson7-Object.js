5871672070eaec709506270f6c1dd30a2475b7a5
unknown <miamby111@gmail.com>
2023-04-06 19:22:02 +0300

    solution of exercises

--- lesson7-Object-Tests/lesson7-Object.js
+++ lesson7-Object-Tests/lesson7-Object.js
@@ -93,8 +93,8 @@ function countProps1(object) {
 }
 countProps1({ mail: 'poly@mail.com', isOnline: true, score: 500 }); // 3
 
-// // // 13. Object.keys
-// // //  Запиши в переменную keys массив ключей собственных свойств объекта apartment, и добавь в массив values все значения его свойств.
+// // //  Object.keys
+// // // 13. Запиши в переменную keys массив ключей собственных свойств объекта apartment, и добавь в массив values все значения его свойств.
 const apartment = {
   descr: 'Spacious apartment in the city center',
   rating: 4,
@@ -195,3 +195,344 @@ function getAllPropValues(propName) {
   // Change code above this line
 }
 console.log(getAllPropValues('name')); // [ 'Radar', 'Scanner', 'Droid', 'Grip' ]
+
+// // // 20. Напиши функцию calculateTotalPrice(productName) которая принимает один параметр productName - название товара. Функция должна вернуть общую стоимость (цена * количество) товара с таким именем из массива products.
+
+const products = [
+  { name: 'Radar', price: 1300, quantity: 4 },
+  { name: 'Scanner', price: 2700, quantity: 3 },
+  { name: 'Droid', price: 400, quantity: 7 },
+  { name: 'Grip', price: 1200, quantity: 9 },
+];
+function calculateTotalPrice(productName) {
+  // Пиши код ниже этой строки
+  for (const product of products) {
+    if (product.name === productName) {
+      return product.price * product.quantity;
+    }
+  }
+  return 0;
+  // Пиши код выше этой строки
+}
+console.log(calculateTotalPrice('Grip')); //10800
+
+// // // Деструктуризация
+// // // 21.
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+// вместо:
+// const yesterday = highTemperatures.yesterday;
+// const today = highTemperatures.today;
+// const tomorrow = highTemperatures.tomorrow;
+// Change code below this line
+const { yesterday, today, tomorrow } = highTemperatures;
+// Change code above this line
+const meanTemperature = (yesterday + today + tomorrow) / 3;
+console.log(meanTemperature);
+
+// // // 22. значение по умолчанию
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+// Change code below this line
+const {
+  yesterday,
+  today,
+  tomorrow,
+  icon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+} = highTemperatures;
+
+// Change code above this line
+const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+// // // 23. При деструктуризации можно изменить имя переменной в которую распаковывается значение свойства.
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+// вместо:
+// const highYesterday = highTemperatures.yesterday;
+// const highToday = highTemperatures.today;
+// const highTomorrow = highTemperatures.tomorrow;
+// const highIcon = highTemperatures.icon;
+// Change code below this line
+const {
+  yesterday: highYesterday,
+  today: highToday,
+  tomorrow: highTomorrow,
+  icon: highIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+} = highTemperatures;
+// Change code above this line
+const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
+
+// // // Для того, чтобы сократить количество повторений, можно деструктуризировать свойства объекта в локальные переменные в теле цикла.
+for (const book of books) {
+  const { title, author, rating } = book;
+  console.log(title);
+  console.log(author);
+  console.log(rating);
+}
+// Если в объекте немного свойств, деструктуризацию можно выполнить прямо в месте объявления переменной book.
+for (const { title, author, rating } of books) {
+  console.log(title);
+  console.log(author);
+  console.log(rating);
+}
+
+// // // 24. Выполни рефакторинг цикла for...of так, чтобы в нём использовалась деструктуризация объекта..
+const colors = [
+  { hex: '#f44336', rgb: '244,67,54' },
+  { hex: '#2196f3', rgb: '33,150,243' },
+  { hex: '#4caf50', rgb: '76,175,80' },
+  { hex: '#ffeb3b', rgb: '255,235,59' },
+];
+const hexColors = [];
+const rgbColors = [];
+// Change code below this line
+// вместо:
+// for (const color of colors) {
+//   hexColors.push(color.hex);
+//   rgbColors.push(color.rgb);
+// }
+for (const { hex, rgb } of colors) {
+  hexColors.push(hex);
+  rgbColors.push(rgb);
+}
+
+// // // 25. Замени объявления всех переменных одной операцией деструктуризации свойств объекта forecast. Задай значение по умолчанию для иконок, переменных todayIcon и tomorrowIcon - строку "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg"
+const forecast = {
+  today: {
+    low: 28,
+    high: 32,
+    icon: 'https://www.flaticon.com/svg/static/icons/svg/861/861059.svg',
+  },
+  tomorrow: {
+    low: 27,
+    high: 31,
+  },
+};
+// const highToday = forecast.today.high;
+// const lowToday = forecast.today.low;
+// const todayIcon = forecast.today.icon;
+
+// const highTomorrow = forecast.tomorrow.high;
+// const lowTomorrow = forecast.tomorrow.low;
+// const tomorrowIcon = forecast.tomorrow.icon;
+
+// Change code below this line
+const {
+  today: {
+    low: lowToday,
+    high: highToday,
+    icon: todayIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+  },
+  tomorrow: {
+    low: lowTomorrow,
+    high: highTomorrow,
+    icon: tomorrowIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+  },
+} = forecast;
+
+// // //  Паттерн «Объект настроек» - объект c именованными свойствами.
+function doStuffWithBook(book) {
+  //some code
+} //где:
+book = {
+  title: 'Последнее королевство',
+  numberOfPages: 736,
+  downloads: 10283,
+  rating: 8.38,
+  public: true,
+};
+// тоже самое:
+doStuffWithBook({
+  title: 'Последнее королевство',
+  numberOfPages: 736,
+  downloads: 10283,
+  rating: 8.38,
+  public: true,
+});
+
+//Ещё один плюс в том, что можно деструктуризировать объект в параметре book.
+// Это можно сделать в теле функции:
+function doStuffWithBook(book) {
+  const { title, numberOfPages, downloads, rating, public } = book;
+  //some code
+}
+// тоже самое: // Или в сигнатуре (подписи), разницы нет.
+function doStuffWithBook({ title, numberOfPages, downloads, rating, public }) {
+  //some code
+}
+
+// // // 26. Функция calculateMeanTemperature(forecast) принимает один параметр forecast - объект температур на два дня следующего формата.
+// Change code below this line
+function calculateMeanTemperature(forecast) {
+  // const todayLow = forecast.today.low;
+  // const todayHigh = forecast.today.high;
+  // const tomorrowLow = forecast.tomorrow.low;
+  // const tomorrowHigh = forecast.tomorrow.high;
+  const {
+    today: { low: todayLow, high: todayHigh },
+    tomorrow: { low: tomorrowLow, high: tomorrowHigh },
+  } = forecast;
+  // Change code above this line
+  return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+}
+console.log(
+  calculateMeanTemperature({
+    today: { low: 28, high: 32 },
+    tomorrow: { low: 25, high: 29 },
+  }),
+); // 28.5
+
+// // // Синтаксис ... (spread) позволяет распылить коллекцию элементов
+//Например, метод Math.max(аргументы) ищет и возвращает самый большой из аргументов (чисел), то есть ожидает не массив значений, а произвольное количество аргументов.
+const temps = [14, -4, 25, 8, 11];
+// В консоли будет массив
+console.log(temps);
+// ❌ Так не сработает, потому что передаём целый массив
+console.log(Math.max(temps)); // NaN
+
+// В консоли будет набор отдельных чисел
+console.log(...temps);
+// ✅ Распылим коллекцию элементов как отдельные аргументы
+console.log(Math.max(...temps)); // 25
+
+//То есть запись:
+Math.max(...[14, -4, 25, 8, 11]);
+//после интерпретации превращается в
+Math.max(14, -4, 25, 8, 11);
+
+// // // 27. Используя распыление и методы Math.max() и Math.min() дополни код так, чтобы в переменной bestScore был самый высокий балл, а в worstScore самый низкий.
+const scores = [89, 64, 42, 17, 93, 51, 26];
+// Change code below this line
+const bestScore = Math.max(...scores);
+const worstScore = Math.min(...scores);
+console.log(bestScore); //93
+console.log(worstScore); //17
+
+// // // Операция ... spread позволяет создать копию массива или «склеить» произвольное количество массивов в один новый. Раньше для этого использовали методы slice() и concat()
+const lastWeekTemps = [14, 25, 11];
+const currentWeekTemps = [23, 17, 18];
+const allTemps = [...lastWeekTemps, ...currentWeekTemps];
+console.log(allTemps); // [14, 25, 11, 23, 17, 18]
+
+// // // 28.
+const firstGroupScores = [64, 42, 93];
+const secondGroupScores = [89, 14, 51, 26];
+const thirdGroupScores = [29, 47, 18, 97, 81];
+// Change code below this line
+const allScores = [...firstGroupScores, ...secondGroupScores, ...thirdGroupScores];
+const bestScore = Math.max(...allScores);
+const worstScore = Math.min(...allScores);
+
+// Операция spread позволяет распылить свойства произвольного количества объектов в один новый. Порядок распыления имеет значение. Имена свойств объекта уникальные, поэтому свойства распыляемого объекта могут перезаписать значение уже существующего свойства, если их имена совпадают.
+const first = { propA: 5, propB: 10, propC: 50 };
+const second = { propC: 15 };
+
+const third = { propB: 20, ...first, ...second };
+console.log(third); // { propA: 5, propB: 10, propC: 15 }
+
+const fourth = { ...first, ...second, propB: 20 };
+console.log(fourth); // { propA: 5, propB: 20, propC: 15 }
+
+const fifth = { ...first, propB: 20, ...second };
+console.log(fifth); // { propA: 5, propB: 20, propC: 15 }
+
+// // // 29. Дополни код так, чтобы в переменной finalSettings получился объект финальных настроек теста.
+const defaultSettings = {
+  theme: 'light',
+  public: true,
+  withPassword: false,
+  minNumberOfQuestions: 10,
+  timePerQuestion: 60,
+};
+const overrideSettings = {
+  public: false,
+  withPassword: true,
+  timePerQuestion: 30,
+};
+// Change code below this line
+const finalSettings = { ...defaultSettings, ...overrideSettings };
+
+// // // 30. Напиши функцию makeTask(data) которая принимает один параметр data - объект со следующими свойствами.
+function makeTask(data) {
+  const completed = false;
+  const category = 'General';
+  const priority = 'Normal';
+  // Change code below this line
+  const newData = { category, priority, ...data, completed };
+  return newData;
+  // Change code above this line
+}
+console.log(
+  makeTask({
+    category: 'Homemade',
+    priority: 'Low',
+    text: 'Take out the trash',
+  }),
+); // { category: "Homemade", priority: "Low", text: "Take out the trash", completed: false }
+
+// // // Операция ... (rest) позволяет собрать группу независимых элементов в новую коллекцию. Синтаксически это близнец операции распыления, но отличить их просто - распыление это когда ... находится в правой части операции присваивания, а сбор это когда ... находится в её левой части.
+// Одна из областей применения операции rest это создание функций которые могут принимать произвольное количество аргументов.
+function multiply(...args) {
+  //Имя параметра может быть произвольным. Чаще всего его называют args, restArgs или otherArgs, сокращённое от arguments.
+  console.log(args); // массив всех аргументов
+}
+multiply(1, 2);
+multiply(1, 2, 3);
+multiply(1, 2, 3, 4);
+
+// // // 31. Используя операцию rest дополни код функции add() так, чтобы она принимала любое количество аргументов, считала и возвращала их сумму.
+// Change code below this line
+function add(...args) {
+  let sum = 0;
+  for (let i = 0; i < args.length; i += 1) {
+    sum += args[i];
+  }
+  return sum;
+  // Change code above this line
+}
+
+// // // Операция ... (rest) также позволяет собрать в массив только ту часть аргументов, которая необходима, объявив параметры до «сбора».
+function multiply(firstNumber, secondNumber, ...otherArgs) {
+  console.log(firstNumber); // Значение первого аргумента
+  console.log(secondNumber); // Значение второго аргумента
+  console.log(otherArgs); // Массив остальных аргументов
+}
+multiply(1, 2);
+multiply(1, 2, 3);
+multiply(1, 2, 3, 4);
+
+// // // 32. Функция addOverNum() считает сумму всех аргументов. Измени параметры и тело функции addOverNum() так, чтобы она считала сумму только тех аргументов, которые больше чем заданное число. Это число должно быть первым параметром функции
+// Change code below this line
+function addOverNum(minNumber, ...args) {
+  let total = 0;
+  for (const arg of args) {
+    if (arg > minNumber) {
+      total += arg;
+    }
+  }
+  return total;
+  // Change code above this line
+}
+
+// // // 32. Функция findMatches() принимает произвольное количество аргументов. Первым аргументом всегда будет массив чисел, а остальные аргументы будут просто числами. Дополни код функции так, чтобы она возвращала новый массив matches, в котором будут только те аргументы, начиная со второго, которые есть в массиве первого аргумента.
+// Change code below this line
+// function findMatches(array = [], ...args)
+function findMatches(array, ...args) {
+  const matches = []; // Don't change this line
+  for (let arg of args) {
+    if (array.includes(arg)) {
+      matches.push(arg);
+    }
+  }
+  // Change code above this line
+  return matches;
+}
